067d1383e5f3935ea8c003f1122e66517861eb15
jdavison33 <Joel.Davison@dataintellect.com>
2024-01-12 10:26:33 +0000

    Update generate.py for right date

--- generate.py
+++ generate.py
@@ -22,9 +22,9 @@ trade_schema = pa.schema([                       # defining schema for trade dat
     pa.field('price', pa.float64()),             # assigning price column float type
     pa.field('size', pa.int64())])               # assigning size column int typoe
 
-trade = ds.dataset(home+"/quack-api/trade.csv", format = "csv", schema=trade_schema) #assigning trade to the arrow dataset generated from the trade csv
-quote = ds.dataset(home+"/quack-api/quote.csv", format = "csv", schema=quote_schema) #assigning quote to the arrow dataset generated from the quote csv
+trade = ds.dataset(home+"/quack-api/2020-01-02/trade.csv", format = "csv", schema=trade_schema) #assigning trade to the arrow dataset generated from the trade csv
+quote = ds.dataset(home+"/quack-api/2020-01-02/quote.csv", format = "csv", schema=quote_schema) #assigning quote to the arrow dataset generated from the quote csv
 
-ds.write_dataset(trade, home+"/quack-api/arrowdb/trade", format="arrow", schema=trade_schema) # writing the trade dataset to disk, saving it in quack-api/arrowdb/trade/ directory 
-ds.write_dataset(quote, home+"/quack-api/arrowdb/quote", format="arrow", schema=quote_schema) # writing the quote dataset to disk, saving it in quack-api/arrowdb/quote/ directory 
+ds.write_dataset(trade, home+"/quack-api/arrowdb/2020-01-02/trade", format="arrow", schema=trade_schema) # writing the trade dataset to disk, saving it in quack-api/arrowdb/2020-01-02/trade/ directory 
+ds.write_dataset(quote, home+"/quack-api/arrowdb/2020-01-02/quote", format="arrow", schema=quote_schema) # writing the quote dataset to disk, saving it in quack-api/arrowdb/2020-01-02/quote/ directory 
 
