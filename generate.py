064d8e773a4bef51446a5d5165e8dc1f7e715892
jdavison33 <Joel.Davison@dataintellect.com>
2024-01-19 13:34:19 +0000

    Add arguments

--- generate.py
+++ generate.py
@@ -2,9 +2,13 @@ import pyarrow as pa
 import pyarrow.dataset as ds
 import duckdb
 import os
-import sys
+import argparse
 
 home=os.environ['HOME'] #setting environment variable
+
+parser=argparse.ArgumentParser(description='Generate csvs to arrow files', epilog='Date flag takes date in the form yyyy-mm-dd')  #
+parser.add_argument('-d','--date')
+args = parser.parse_args()
 
 quote_schema = pa.schema([                       # defining schema for quote dataset, this should include all the columns from the csv that you are generating from
     pa.field('time', pa.timestamp('ns')),        # assigning time column timestamp type
@@ -24,9 +28,9 @@ trade_schema = pa.schema([                       # defining schema for trade dat
 
 #If using other data that isn't trade or quote data, the schema will look different, and should include the column of that data, with their desired types
 
-trade = ds.dataset(home+"/quack-api/"+str(sys.argv[1])+"/trade.csv", format = "csv", schema=trade_schema) #assigning trade to the arrow dataset generated from the trade csv, using the trade schema assigned above
-quote = ds.dataset(home+"/quack-api/"+str(sys.argv[1])+"/quote.csv", format = "csv", schema=quote_schema) #assigning quote to the arrow dataset generated from the quote csv, using the quote schema assigned above
+trade = ds.dataset(home+"/quack-api/"+str(args.date)+"/trade.csv", format = "csv", schema=trade_schema) #assigning trade to the arrow dataset generated from the trade csv, using the trade schema assigned above
+quote = ds.dataset(home+"/quack-api/"+str(args.date)+"/quote.csv", format = "csv", schema=quote_schema) #assigning quote to the arrow dataset generated from the quote csv, using the quote schema assigned above
 
-ds.write_dataset(trade, home+"/quack-api/arrowdb/trade/"+str(sys.argv[1]), format="arrow", schema=trade_schema) # writing the trade dataset to disk, saving it in quack-api/arrowdb/2020-01-02/trade/ directory 
-ds.write_dataset(quote, home+"/quack-api/arrowdb/quote/"+str(sys.argv[1]), format="arrow", schema=quote_schema) # writing the quote dataset to disk, saving it in quack-api/arrowdb/2020-01-02/quote/ directory 
+ds.write_dataset(trade, home+"/quack-api/arrowdb/trade/"+str(args.date), format="arrow", schema=trade_schema) # writing the trade dataset to disk, saving it in quack-api/arrowdb/2020-01-02/trade/ directory 
+ds.write_dataset(quote, home+"/quack-api/arrowdb/quote/"+str(args.date), format="arrow", schema=quote_schema) # writing the quote dataset to disk, saving it in quack-api/arrowdb/2020-01-02/quote/ directory 
 
